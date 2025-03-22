fd5ad1270e11bb24fef5899d7aa8d3bf59f864c6
Hari <140445632+hari-init@users.noreply.github.com>
2025-03-21 21:46:25 -0400

    updated Test cases

--- frontend/app/components/TeamTable/TeamTable.test.tsx
+++ frontend/app/components/TeamTable/TeamTable.test.tsx
@@ -5,26 +5,6 @@ import TeamTable from "./TeamTable";
 import "@testing-library/jest-dom";
 
 describe("TeamTable Component", () => {
-  const mockTeams = [
-    {
-      id: "1",
-      name: "Team A",
-      nickname: "TA",
-      display_name: "Team Alpha",
-      conference: "East",
-      division: "North",
-      logo: "/logo-team-a.png",
-    },
-    {
-      id: "2",
-      name: "Team B",
-      nickname: "TB",
-      display_name: "Team Beta",
-      conference: "West",
-      division: "South",
-    },
-  ];
-
   it("renders the table headers when isHead is true", () => {
     render(<TeamTable isHead={true} list={[]} />);
 
